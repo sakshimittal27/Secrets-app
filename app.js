2e2f2b6f9ef017c389d4c6497a88c42dbe708dc8
sakshimittal27 <sakshil271099@gmail.com>
2020-07-27 09:03:07 +0530

    Port updated

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "https://secretsapp1.herokuapp.com/auth/google/secrets",
+    callbackURL: "/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
   function(accessToken, refreshToken, profile, cb) {

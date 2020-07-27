f313c7a95f5c21910e77f56a1a7494bdb650c340
sakshimittal27 <sakshil271099@gmail.com>
2020-07-27 11:01:18 +0530

    .env file update

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.use(session({
-    secret: 'keyboard cat',
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false 
 }));
